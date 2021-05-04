95933c5d4c07a284b48e040a2873b7a02e76fcde
JaneyR <rapley.jane@gmail.com>
2021-05-04 18:04:59 -0400

    homework 3 partially done. most of java written need to comeback and finish

--- 02-Homework/Develop/script.js
+++ 02-Homework/Develop/script.js
@@ -1,14 +1,81 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var alphaCap = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+var specialCharacters = ["!", "&", "^", "(", ")", "%", "%", "#"];
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+
+
   passwordText.value = password;
 
 }
 
+function generatePassword() {
+  var options = getOptions()
+
+  var result = [];
+  var possibleCharacters = [];
+  var garCharacters = [];
+
+  if (options.hasLowercase) {
+    possibleCharacters = possibleCharacters.concat(alphaLower)
+    garCharacters.push(getRandom(alphaLower))
+  }
+
+
+}
+
+function getRandom(arr) {
+  var ranIndex = Math.floor(Math.random() * arr.length)
+  var ranElement = arr[ranIndex]
+
+  return ranElement
+}
+
+
+function getOptions() {
+  var length = parseInt(prompt("How many characters would you like?"));
+
+  if (isNaN(length) === true) {
+    alert("Please enter a number")
+    return
+  }
+
+  if (length < 8 || length > 128) {
+    alert("Password length must be between 8-128 characters")
+    return
+  }
+
+  var hasUppercase = confirm("Do you want uppercase characters?");
+  var hasLowercase = confirm("Do you want lowercase characters?");
+  var hasNumbers = confirm("Do you want numbers?");
+  var hasSpecial = confirm("Do you want special characters?");
+
+  if (
+    hasUppercase === false &&
+    hasLowercase === false &&
+    hasNumbers === false &&
+    hasSpecial === false 
+  ) {
+    alert("You must select at least one option before continuing")
+    return
+  }
+
+  var passwordOptions = {
+    length: length,
+    hasUppercase: hasUppercase,
+    hasLowercase: hasLowercase,
+    hasNumbers: hasNumbers,
+    hasSpecial: hasSpecial,
+  }
+
+  return passwordOptions 
+}
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
